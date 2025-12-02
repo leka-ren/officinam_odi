f5587b22dfedff52ad6cfe3bd04e1e3ddd72dc4a
leka-ren <polafort@gmail.com>
2025-12-02 13:47:28 +0300

    released server and handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "Log: ", log.Ldate|log.Ltime)
+
+	serverLogger := server.NewServer(logger)
+
+	err := serverLogger.Start()
+
+	if err != nil {
+		logger.Fatalf("Fatal server run: %v", err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,60 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func StartPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(res, "method GET only available", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(res, req, "index.html")
+}
+
+func UploadForm(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "method POST only available", http.StatusMethodNotAllowed)
+	}
+
+	err := req.ParseMultipartForm(5 << 20)
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+
+	file, handler, err := req.FormFile("myFile")
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+
+	defer file.Close()
+
+	dataFromFile, err := io.ReadAll(file)
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+
+	convertedData := service.Сonversion(string(dataFromFile))
+
+	fileName := time.Now().UTC().String() + filepath.Ext(handler.Filename)
+
+	_, err = os.Create(fileName)
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(convertedData))
+}
